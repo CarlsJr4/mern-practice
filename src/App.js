6bc8a39d0bf782ecb1856fb3a4c1a9b3f337226f
Carl Dungca <carl.dungca@yahoo.com>
2020-04-01 14:56:00 -0700

    Added effect hook to app to retrieve all psots

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './stylesheets/App.css';
 import PostForm from './components/PostForm';
 import AllPosts from './components/AllPosts';
 
-// Your goal is to build a form that submits posts to the app
-// Today's goal: submit form data to your backend
+// Today's goal: Display posts from backend to frontend
 function App() {
+	const [posts, updatePosts] = useState([]);
+
+	async function getPosts() {
+		const allPosts = await axios.get('http://localhost:3000/api/posts');
+		updatePosts(allPosts.data);
+		console.log(posts);
+	}
+
+	useEffect(() => {
+		getPosts()
+	}, [posts.length])
+
   return (
     <div className="App">
 			<h1>Today's posts:</h1>
