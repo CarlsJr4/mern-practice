47b7c38065d1e16ed99815afa7b31faac42faa11
Carl Dungca <carl.dungca@yahoo.com>
2020-04-01 12:04:51 -0700

    Made PostForm a controlled component

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,15 +1,58 @@
 import React, { useState } from 'react';
 
 export default function PostForm() {
+	const [formData, setFormData] = useState({
+		title: '',
+		author: '',
+		body: ''
+	});
+
+	function handleInputChange(e) {
+		const target = e.target; // Input being updated
+		const {name, value} = target // Extract the name and value of your target
+
+		setFormData({
+			...formData,
+			[name]: value
+		})
+	}
+
 	return (
-		<form action="">
-			<label htmlFor="title">Title:</label>
-			<input id="title" type="text"/>
-			<label htmlFor="author">Author:</label>
-			<input id="author" type="text"/>
-			<label htmlFor="text">Text:</label>
-			<textarea id="text" type="text"/>
-			<input type="submit" value="Submit"/>
+		<form action="/api/posts" method="post">
+			<label 
+				htmlFor="title">
+					Title:
+			</label>
+			<input 
+				id="title" 
+				name="title" 
+				type="text" 
+				onChange={handleInputChange}
+			/>
+			<label 
+				htmlFor="author">
+					Author:
+			</label>
+			<input 
+				id="author"
+				name="author" 
+				type="text" 
+			  onChange={handleInputChange}
+			/>
+			<label 
+				htmlFor="text">
+					Text:
+			</label>
+			<textarea
+				id="text"
+				name="body" 
+				type="text" 
+				onChange={handleInputChange}
+			/>
+			<input 
+				type="submit" 
+				value="Submit"
+			/>
 		</form>
 	)
 }
