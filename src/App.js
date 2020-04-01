f63cd23c7789fb02c73e2af8fc1a4b105c6a5082
Carl Dungca <carl.dungca@yahoo.com>
2020-04-01 12:04:33 -0700

    Added AllPosts component to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './stylesheets/App.css';
 import PostForm from './components/PostForm';
+import AllPosts from './components/AllPosts';
 
 // Your goal is to build a form that submits posts to the app
+// Today's goal: submit form data to your backend
 function App() {
   return (
     <div className="App">
+			<h1>Today's posts:</h1>
+			<AllPosts />
 			<h1>Make a post:</h1>
 			<PostForm />
     </div>
