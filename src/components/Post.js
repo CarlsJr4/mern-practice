ed719fbb34c0db4126b01241fd17c0bd72ed0def
Carl Dungca <carl.dungca@yahoo.com>
2020-04-02 11:36:16 -0700

    Added placeholder post component

---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Post() {
+	return (
+		<div className="App">
+			<h1>This is a blog post!</h1>
+			{/* We need the title, author, and content here */}
+			{/* How do we identify which post was clicked? */}
+		</div>
+	)
+}
