1459d261f9c23771012833226d89992e320561ed
Carl Dungca <carl.dungca@yahoo.com>
2020-04-02 11:28:18 -0700

    Refactored default app page into its own component as a route

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import axios from 'axios';
 import './stylesheets/App.css';
-import PostForm from './components/PostForm';
-import AllPosts from './components/AllPosts';
+import Home from './components/Home';
 
 // Today's goal: Click on blog posts to view them
 // Process:
@@ -15,13 +14,13 @@ import AllPosts from './components/AllPosts';
 // 2. Provide links - the links will live on the blog post titles
 // 3. Provide a switch that renders the first component that matches the current URL 
 
+// The point of this component is to hold state and routes
 function App() {
 	const [posts, updatePosts] = useState([]);
 
 	// Retrieve all posts upon mounting of the app
 	useEffect(() => {
-		// Need to customize app incase of a backend server failure
-		// How will we handle these errors?
+		// Need to customize app errors incase of a backend server failure
 		async function getPosts() {
 			const allPosts = await axios.get('http://localhost:3000/api/posts');
 			updatePosts(allPosts.data);
@@ -33,12 +32,7 @@ function App() {
 		<Router>
 			<Switch>
 				<Route path="/">
-				<div className="App">
-					<h1>Make a post:</h1>
-					<PostForm updatePosts={updatePosts} posts={posts} />
-					<h1>Today's posts:</h1>
-					<AllPosts data={posts} />
-				</div>
+					<Home posts={posts} updatePosts={updatePosts}/>
 				</Route>
 				<Route path="/post">
 					{/* Insert a blog post page here */}
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PostForm from './PostForm';
+import AllPosts from './AllPosts';
+
+// The point of this component is to hold the home route and display relevant data
+export default function Home({updatePosts, posts}) {
+	return (
+		<div className="App">
+			<h1>Make a post:</h1>
+			<PostForm updatePosts={updatePosts} posts={posts} />
+			<h1>Today's posts:</h1>
+			<AllPosts data={posts} />
+		</div>
+	)
+}
+
