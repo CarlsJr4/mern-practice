2ed150c6d2af5bf3d5f968827a55823270562df4
Carl Dungca <carl.dungca@yahoo.com>
2020-04-04 13:48:03 -0700

    Added cleanup effect to the auth page that removes the JWT in localstorage

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Login from './Login';
 import Register from './Register';
 
 export default function Auth({handleAuth}) {
+	// Clear JWT when this page loads so that nonauthenticated users cannot access it
+	useEffect(() => {
+		localStorage.removeItem('jwt');
+	})
+
 	return (
 		<div className="App">
 			<h1>Welcome to the blog!</h1>
